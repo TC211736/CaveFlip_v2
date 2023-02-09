6c75e6c450cd00e601d02d4293786f31d47f7502
TC211736 <TC211736@reigate.ac.uk>
2023-02-09 16:15:31 +0000

    Finished Tutorial, began animations Start SQL now pls future Toby

---
+++ core/src/com/mygdx/game/states/pauseState.java
@@ -0,0 +1,30 @@
+package com.mygdx.game.states;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class pauseState extends State{
+
+    protected pauseState(GameStateManager gsm) {
+        super(gsm);
+    }
+
+    @Override
+    protected void handleInput() {
+
+    }
+
+    @Override
+    public void update(float dt) {
+
+    }
+
+    @Override
+    public void render(SpriteBatch sb) {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
